80c90917a1b855861564a655be809ad797812d90
sfhart33 <sfhart33@gmail.com>
2023-08-01 16:16:17 -0700

    Sync with outputs

---
+++ inputs/download_data.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Download NCBI-annotated Mya arenaria genome
+    wget --recursive --no-host-directories --cut-dirs=6 ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/914/265/GCF_026914265.1_ASM2691426v1/ -P $INPUT_FOLDER/
+
+# Download RNAseq data from SRA using SRA toolkit
+        # Download installation instructions: https://github.com/ncbi/sra-tools/wiki
+        # helpful notes on usage https://www.reneshbedre.com/blog/ncbi_sra_toolkit.html
+    module load sratools # version 2.10.4
+
+    #test download (for timing)
+        cd $INPUT_FOLDER/fastq
+        fasterq-dump SRR23856978
+        gzip SRR23856978_1.fastq &
+        gzip SRR23856978_2.fastq &
+        wait
+
+    # Full download (takes about an hour each  - will take about 28hr)
+        cd $INPUT_FOLDER/fastq
+        ACCESSIONS="SRR22741477
+            SRR22741478
+            SRR22741479
+            SRR22741480
+            SRR22741481
+            SRR22741482
+            SRR23856977
+            SRR23856979
+            SRR23856980
+            SRR23856981
+            SRR23856982
+            SRR23856983
+            SRR23856984
+            SRR23856985
+            SRR23856986
+            SRR23856987
+            SRR23856988
+            SRR23856989
+            SRR23856990
+            SRR23856991
+            SRR23856992
+            SRR23856993
+            SRR23856994
+            SRR23856995"
+        TOTAL=$(printf '%s\n' $ACCESSIONS:q | wc -w)
+        COUNT=0
+        for ACCESSION in $ACCESSIONS
+        do
+            echo Downloading $ACCESSION
+            COUNT=$((COUNT+1))
+            fasterq-dump $ACCESSION
+            echo Zipping $ACCESSION
+            gzip $ACCESSION"_1.fastq" &
+            gzip $ACCESSION"_2.fastq" &
+            echo $COUNT complete out of $TOTAL
+        done
+        wait
+
+########################################################
+# Note that this does not yet incluse ASW or PEI samples
+########################################################
