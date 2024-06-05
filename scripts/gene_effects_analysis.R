0dd8b3166b8fd7696d8c7303cfaad98e46b0a495
sfhart33 <sfhart33@gmail.com>
2024-06-04 17:21:48 -0700

    alter gene effects script to drop PEI and redo steamer analysis

--- scripts/gene_effects_analysis.R
+++ scripts/gene_effects_analysis.R
@@ -50,9 +50,9 @@
     USA_BTN_clams <- filter(samples_data, tissue == "BTN", location == "USA") %>%
         pull(clam) %>%
         as.character()    
-    PEI_BTN_clams <- filter(samples_data, tissue == "BTN", location == "PEI")  %>%
-        pull(clam) %>%
-        as.character()
+#    PEI_BTN_clams <- filter(samples_data, tissue == "BTN", location == "PEI")  %>%
+#        pull(clam) %>%
+#        as.character()
 
 # Load data and count number shared by all cancers
   for (sample in samples_names){
@@ -81,23 +81,23 @@
     cancer_shared <- fusions_file %>%
         select(all_of(BTN_clams)) %>%
         filter_all(all_vars(. > 0)) %>%
-        nrow() # 53 in all BTN
+        nrow() # 212 in all BTN
 
-    usabtn_shared <- fusions_file %>%
-        select(all_of(USA_BTN_clams)) %>%
-        filter_all(all_vars(. > 0)) %>%
-        nrow() # 212 in all USA BTN
+#    usabtn_shared <- fusions_file %>%
+#        select(all_of(USA_BTN_clams)) %>%
+#        filter_all(all_vars(. > 0)) %>%
+#        nrow() # 212 in all USA BTN
 
-    usapei_shared <- fusions_file %>%
-        filter(BTN_6> 0) %>% # update with other PEI
-        select(all_of(USA_BTN_clams)) %>%
-        filter_all(any_vars(. > 0)) %>%
-        nrow() # 91 in any USA BTN and PEI BTN
+#    usapei_shared <- fusions_file %>%
+#        filter(BTN_6> 0) %>% # update with other PEI
+#        select(all_of(USA_BTN_clams)) %>%
+#        filter_all(any_vars(. > 0)) %>%
+#        nrow() # 91 in any USA BTN and PEI BTN
 
     cancer_shared_nohealthy <- fusions_file %>%
         filter_at(vars(starts_with("BTN")), all_vars(. > 0)) %>%
         filter_at(vars(starts_with("HEL")), all_vars(. == 0)) %>%
-        nrow() # 31 in all BTN no healthy 
+        nrow() # 181 in all BTN no healthy 
 
     usabtn_shared_nohealthy <- fusions_file %>%
         filter_at(vars(starts_with("HEL")), all_vars(. == 0)) %>%
@@ -108,12 +108,12 @@
     all_sample_fusions <- fusions_file %>%
         select(all_of(samples_clams)) %>%
         filter_all(all_vars(. > 0)) %>%
-        nrow() # 15 in all samples - 16 before PEI
+        nrow() # 16 in all samples
 
     all_healthy_fusions <- fusions_file %>%
         select(all_of(heme_clams)) %>%
         filter_all(all_vars(. > 0)) %>%
-        nrow() # 16 in all healthies - 17 before PEI
+        nrow() # 16 in all healthies
 
 # Tally up total fusions per sample
     fusion_counts <- fusions_file %>%
@@ -133,15 +133,14 @@
 # Correct for fusions in all samples
     fusion_counts_adj <- mutate(fusion_counts, adj.count = as.numeric(count) - all_sample_fusions)
     fusion_counts_mean_adj <- fusion_counts_adj %>%
-        group_by(group) %>%
+        group_by(type) %>%
         summarize(mean = mean(adj.count),
                 sd = sd(adj.count))
     #fusion_counts_mean$shared <- c(cancer_shared,all_healthy_fusions)
-    fusion_counts_mean_adj$shared <- c(0, cancer_shared - all_sample_fusions, cancer_shared - all_sample_fusions)
-    fusion_counts_mean_adj$usa <- c(0, 0, usabtn_shared - all_sample_fusions)
+    fusion_counts_mean_adj$shared <- c(cancer_shared - all_sample_fusions, 0)
 
 # t-test
-    t.test(as.numeric(filter(fusion_counts, type=="healthy")[,"count"]),as.numeric(filter(fusion_counts, type=="BTN")[,"count"])) # p-value = 0.0001003
+    t.test(as.numeric(filter(fusion_counts, type=="healthy")[,"count"]),as.numeric(filter(fusion_counts, type=="BTN")[,"count"])) # p-value = 0.0001548
     # t.test(as.numeric(filter(fusion_counts, group=="USA BTN")[,"count"]),as.numeric(filter(fusion_counts, group=="PEI BTN")[,"count"])) # not enough 'y' observations
     
 
@@ -169,13 +168,12 @@
 
 # plot
   pdf(paste0(output_folder,"/plots/fusion_plot.pdf"))
-        ggplot(fusion_counts_mean_adj, aes(x=group,y=mean)) +
+        ggplot(fusion_counts_mean_adj, aes(x=type,y=mean)) +
             geom_bar(position="dodge", stat="identity", fill = "#969696")+
-            geom_bar(aes(x=group,y=usa),position="dodge", stat="identity", fill = "#515151")+
-            geom_bar(aes(x=group,y=shared),position="dodge", stat="identity", fill = "black")+
+            geom_bar(aes(x=type,y=shared),position="dodge", stat="identity", fill = "black")+
             geom_errorbar(aes(ymin=(mean-sd), ymax=(mean+sd)), width=.2,position=position_dodge(.9))+
             geom_point(data=fusion_counts_adj,
-                        aes(x=group,y=as.numeric(adj.count)),
+                        aes(x=type,y=as.numeric(adj.count)),
                         position=position_dodge(.9), size=2)+
             xlab(NULL)+
             ylab("Number of fusion transcripts\n(minus fusions in all samples)")+
@@ -209,25 +207,35 @@ setwd(output_folder)
     head(gene_names)
 
 # Load expression data and merge
-    load(paste0(output_folder,"/data_tables/heme_vs_usabtn_results.rda")) # heme_uBTN: negative is higher in BTN
-    usa_exp <- as.data.frame(heme_uBTN) %>%
+#    load(paste0(output_folder,"/data_tables/heme_vs_usabtn_results.rda")) # heme_uBTN: negative is higher in BTN
+#    usa_exp <- as.data.frame(heme_uBTN) %>%
+#        rownames_to_column(var = "gene") %>%
+#        filter(baseMean > 1) %>%
+#        select(gene, log2FoldChange) %>%
+#        inner_join(usa_calls) %>%
+#        mutate(sublin = "USA")
+#    load(paste0(output_folder,"/data_tables/heme_vs_peibtn_results.rda")) # heme_pBTN: negative is higher in BTN    
+#    pei_exp <- as.data.frame(heme_pBTN) %>%
+#        rownames_to_column(var = "gene") %>%
+#        filter(baseMean > 1) %>%
+#        select(gene, log2FoldChange) %>%
+#        inner_join(pei_calls) %>%
+#        mutate(sublin = "PEI")
+
+#    cn_exp <- rbind(usa_exp, pei_exp)
+
+    load(paste0(output_folder,"/data_tables/heme_vs_btn_results.rda")) # heme_BTN: negative is higher in BTN
+    cn_exp <- as.data.frame(heme_BTN) %>%
         rownames_to_column(var = "gene") %>%
         filter(baseMean > 1) %>%
         select(gene, log2FoldChange) %>%
         inner_join(usa_calls) %>%
         mutate(sublin = "USA")
-    load(paste0(output_folder,"/data_tables/heme_vs_peibtn_results.rda")) # heme_pBTN: negative is higher in BTN    
-    pei_exp <- as.data.frame(heme_pBTN) %>%
-        rownames_to_column(var = "gene") %>%
-        filter(baseMean > 1) %>%
-        select(gene, log2FoldChange) %>%
-        inner_join(pei_calls) %>%
-        mutate(sublin = "PEI")
 
-    cn_exp <- rbind(usa_exp, pei_exp)
+
 
 pdf(paste0(output_folder,"/plots/cn_exp_plot.pdf"))
-    ggplot(cn_exp, aes(cn, -log2FoldChange, fill = sublin)) +
+    ggplot(cn_exp, aes(cn, -log2FoldChange)) +
         geom_hline(yintercept = 0) +  
         geom_boxplot(outlier.shape = NA) +
         ylim(-12,12) +
@@ -250,15 +258,9 @@ dev.off()
         left_join(
             dplyr::select(
                 rownames_to_column(
-                    as.data.frame(heme_uBTN), var = "gene"
+                    as.data.frame(heme_BTN), var = "gene"
                 ), gene, u_lfc = log2FoldChange, u_padj = padj)
             ) %>%         
-        left_join(
-            dplyr::select(
-                rownames_to_column(
-                    as.data.frame(heme_pBTN), var = "gene"
-                ), gene, p_lfc = log2FoldChange, p_padj = padj)
-            ) %>%
         # mutate(
         #     U_up = ifelse(u_lfc < 0, TRUE, FALSE),
         #     U_dn = ifelse(u_lfc > 0, TRUE, FALSE),
@@ -267,8 +269,8 @@ dev.off()
         mutate(
             U_up = ifelse(u_padj < 0.05 & u_lfc < 0, TRUE, FALSE),
             U_dn = ifelse(u_padj < 0.05 & u_lfc > 0, TRUE, FALSE),
-            P_up = ifelse(p_padj < 0.05 & p_lfc < 0, TRUE, FALSE),
-            P_dn = ifelse(p_padj < 0.05 & p_lfc > 0, TRUE, FALSE),
+#            P_up = ifelse(p_padj < 0.05 & p_lfc < 0, TRUE, FALSE),
+#            P_dn = ifelse(p_padj < 0.05 & p_lfc > 0, TRUE, FALSE),
             PEI = ifelse(group %in% c("allCnoH","allPEI"), TRUE, FALSE),
             USA = ifelse(group %in% c("allCnoH","allUSA"), TRUE, FALSE))
 
@@ -279,25 +281,21 @@ dev.off()
         summarise(
             up=sum(U_up, na.rm = TRUE),
             dn=sum(U_dn, na.rm = TRUE),
-            up_con=sum(P_up, na.rm = TRUE),
-            dn_con=sum(P_dn, na.rm = TRUE),
-            n=n()
-            ) # %>%
-            # mutate(
-            #     U_up_frac = U_up/(U_up+U_dn),
-            #     P_up_frac = P_up/(P_up+P_dn)
-            # )
-
-    steamer_upstream_PEI <- steamer_ins %>%
-        filter(inserted == "2kbup") %>%
-        group_by(strand, PEI) %>%
-        summarise(
-            up=sum(P_up, na.rm = TRUE),
-            dn=sum(P_dn, na.rm = TRUE),
-            up_con=sum(U_up, na.rm = TRUE),
-            dn_con=sum(U_dn, na.rm = TRUE),
+#            up_con=sum(P_up, na.rm = TRUE),
+#            dn_con=sum(P_dn, na.rm = TRUE),
             n=n()
             )
+
+#    steamer_upstream_PEI <- steamer_ins %>%
+#        filter(inserted == "2kbup") %>%
+#        group_by(strand, PEI) %>%
+#        summarise(
+#            up=sum(P_up, na.rm = TRUE),
+#            dn=sum(P_dn, na.rm = TRUE),
+#            up_con=sum(U_up, na.rm = TRUE),
+#            dn_con=sum(U_dn, na.rm = TRUE),
+#            n=n()
+#            )
 
     steamer_genes_USA <- steamer_ins %>%
         filter(inserted == "genes") %>%
@@ -305,46 +303,97 @@ dev.off()
         summarise(
             up=sum(U_up, na.rm = TRUE),
             dn=sum(U_dn, na.rm = TRUE),
-            up_con=sum(P_up, na.rm = TRUE),
-            dn_con=sum(P_dn, na.rm = TRUE),
+#            up_con=sum(P_up, na.rm = TRUE),
+#            dn_con=sum(P_dn, na.rm = TRUE),
             n=n()
             )
 
-    steamer_genes_PEI <- steamer_ins %>%
-        filter(inserted == "genes") %>%
-        group_by(PEI) %>%
-        summarise(
-            up=sum(P_up, na.rm = TRUE),
-            dn=sum(P_dn, na.rm = TRUE),
-            up_con=sum(U_up, na.rm = TRUE),
-            dn_con=sum(U_dn, na.rm = TRUE),
-            n=n()
-            )
+#    steamer_genes_PEI <- steamer_ins %>%
+#        filter(inserted == "genes") %>%
+#        group_by(PEI) %>%
+#        summarise(
+#            up=sum(P_up, na.rm = TRUE),
+#            dn=sum(P_dn, na.rm = TRUE),
+#            up_con=sum(U_up, na.rm = TRUE),
+#            dn_con=sum(U_dn, na.rm = TRUE),
+#            n=n()
+#            )
 
 # steamer_upstream_USA
 # steamer_upstream_PEI
 # steamer_genes_USA
 # steamer_genes_PEI
 
+#### NEW boxplot of LFC based on steamer insertion status
+
+pdf(paste0(output_folder,"/plots/steamer_exp_plot.pdf"))
+    ggplot(steamer_ins, aes(inserted, -u_lfc, fill = USA)) +
+        geom_hline(yintercept = 0) +  
+        geom_boxplot(outlier.shape = NA) +
+        #ylim(-12,12) +
+        ylab("log2(foldchange) in BTN vs hemocytes")+
+	xlab("Present in samples")+
+            theme_classic() +
+            theme(axis.text=element_text(size=12,face="bold"),
+                    axis.title=element_text(size=16,face="bold"),
+                    text=element_text(size=18,face="bold"),
+                    legend.title = element_blank())
+    ggplot(steamer_ins, aes(inserted, -u_lfc, fill = USA, color = strand)) +
+        geom_hline(yintercept = 0) +  
+        geom_boxplot(outlier.shape = NA) +
+	scale_fill_manual(values = c("green","orange")) +
+        #ylim(-12,12) +
+        ylab("log2(foldchange) in BTN vs hemocytes")+
+	xlab("Present in samples")+
+            theme_classic() +
+            theme(axis.text=element_text(size=12,face="bold"),
+                    axis.title=element_text(size=16,face="bold"),
+                    text=element_text(size=18,face="bold"),
+                    legend.title = element_blank())
+
+    ggplot(steamer_ins, aes(inserted, -u_lfc, fill = USA)) +
+        geom_hline(yintercept = 0) +  
+        geom_violin() +
+        #ylim(-12,12)
+        ylab("log2(foldchange) in BTN vs hemocytes")+
+	xlab("Present in samples")+
+            theme_classic() +
+            theme(axis.text=element_text(size=12,face="bold"),
+                    axis.title=element_text(size=16,face="bold"),
+                    text=element_text(size=18,face="bold"),
+                    legend.title = element_blank())
+
+dev.off()
+
+t.test(filter(steamer_ins, inserted=="2kbup", USA==TRUE)[,"u_lfc"],filter(steamer_ins, inserted=="2kbup", USA==FALSE)[,"u_lfc"]) # p-value = 0.1623
+t.test(filter(steamer_ins, inserted=="2kbup", USA==TRUE)[,"u_lfc"]) # p-value = 0.01015
+t.test(filter(steamer_ins, inserted=="2kbup", USA==FALSE)[,"u_lfc"]) # p-value = 0.5117
+wilcox.test(filter(steamer_ins, inserted=="2kbup", USA==TRUE)[,"u_lfc"],filter(steamer_ins, inserted=="2kbup", USA==FALSE)[,"u_lfc"]) # p-value = 0.0384
+
+t.test(filter(steamer_ins, inserted=="genes", USA==TRUE)[,"u_lfc"],filter(steamer_ins, inserted=="genes", USA==FALSE)[,"u_lfc"]) # p-value = 0.385
+t.test(filter(steamer_ins, inserted=="genes", USA==TRUE)[,"u_lfc"]) # p-value = 0.02075
+t.test(filter(steamer_ins, inserted=="genes", USA==FALSE)[,"u_lfc"]) # p-value = 0.06387
+wilcox.test(filter(steamer_ins, inserted=="genes", USA==TRUE)[,"u_lfc"],filter(steamer_ins, inserted=="genes", USA==FALSE)[,"u_lfc"]) # p-value = 0.2679
+
 # Print output tables
     # Did not plot since there doesn't appear to be a significant effect of steamer with the new samples
-    cbind(steamer_genes_USA[,1],
-        steamer_genes_USA[,-1] + steamer_genes_PEI[,-1]) %>%
-        mutate(
-            up_frac = up/(up+dn),
-            up_frac_con = up_con/(up_con+dn_con),
-            up_frac2 = up/n,
-            up_frac_con2 = up_con/n
-        ) %>%
-        write.table(paste0(output_folder,"/data_tables/steamer_ins_in_gene_effect.tsv"),
-                    sep="\t", row.names = TRUE, quote = FALSE)
-    cbind(ungroup(steamer_upstream_USA)[,1:2],
-        steamer_upstream_USA[,-c(1,2)] + steamer_upstream_PEI[,-c(1,2)]) %>%
-        mutate(
-            up_frac = up/(up+dn),
-            up_frac_con = up_con/(up_con+dn_con),
-            up_frac2 = up/n,
-            up_frac_con2 = up_con/n
-        ) %>%
-        write.table(paste0(output_folder,"/data_tables/steamer_ins_upstream_gene_effect.tsv"),
-                    sep="\t", row.names = TRUE, quote = FALSE)
+#    cbind(steamer_genes_USA[,1],
+#        steamer_genes_USA[,-1] + steamer_genes_PEI[,-1]) %>%
+#        mutate(
+#            up_frac = up/(up+dn),
+#            up_frac_con = up_con/(up_con+dn_con),
+#            up_frac2 = up/n,
+#            up_frac_con2 = up_con/n
+#        ) %>%
+#        write.table(paste0(output_folder,"/data_tables/steamer_ins_in_gene_effect.tsv"),
+#                    sep="\t", row.names = TRUE, quote = FALSE)
+#    cbind(ungroup(steamer_upstream_USA)[,1:2],
+#        steamer_upstream_USA[,-c(1,2)] + steamer_upstream_PEI[,-c(1,2)]) %>%
+#        mutate(
+#            up_frac = up/(up+dn),
+#            up_frac_con = up_con/(up_con+dn_con),
+#            up_frac2 = up/n,
+#            up_frac_con2 = up_con/n
+#        ) %>%
+#        write.table(paste0(output_folder,"/data_tables/steamer_ins_upstream_gene_effect.tsv"),
+#                    sep="\t", row.names = TRUE, quote = FALSE)
